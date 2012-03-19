a3a54fbdabe80551d9c90b749c6bad0e920c400a
FND <fnd@fnd-oda.localdomain>
2012-03-19 18:00:35 +0100

    indexed checkboxes by row number
    this allows the server to properly associate their state to the
    remaining form data

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@
 $("form table").on("change", "tr:last input", function(ev) {
 	var el = $(this),
 		currentRow = el.closest("tr"),
-		newRow = el.closest("table").find("tr:last").clone(); // NB: won't work in IE - cf. http://bugs.jquery.com/ticket/9777
-	newRow.html(newRow.html()). // ensures pristine state
-		insertAfter(currentRow);
+		table = el.closest("table"),
+		newRow = $("tr:last", table).clone(), // NB: won't work in IE - cf. http://bugs.jquery.com/ticket/9777
+		rowCount = $("tr", table).length;
+
+	newRow.html(newRow.html()); // ensures pristine state
+	// update checkbox identifiers
+	$("input[type=checkbox]", newRow).each(function(i, node) {
+		$(node).val(rowCount - 1);
+	});
+
+	newRow.insertAfter(currentRow);
 });
